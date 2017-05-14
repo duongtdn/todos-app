61d26431421fbca9c8a0505cd3a7bbcf5e605776
duongtdn <duongtdn@gmail.com>
2017-05-14 20:29:42 +0700

    manually request ad every 60s

--- www/src/admob.js
+++ www/src/admob.js
@@ -14,6 +14,11 @@ if( /(android)/i.test(navigator.userAgent) ) {
     };
 }
 
+/* Define timeout for ad request */
+const TO = 60000; // 60s
+
+let ts = 0; // global captured timestamp in each time ad request is invoked
+
 export default {
 
   getEvents(){
@@ -25,7 +30,8 @@ export default {
   },
 
   initAdmob() {
-    if (admob) {      
+    if (admob) {       
+        /* setup admob options */    
         admob.setOptions({ 
           publisherId: admobid.banner, 
           autoShowBanner: true,
@@ -35,23 +41,69 @@ export default {
           offsetStatusBar: true,
           isTesting: false, 
         });
+        /* register event for manual request by destroy and create banner
+           when ad is loaded, setup a timeout for request ad */
+        // document.addEventListener(admob.events.onAdLoaded, this.scheduleNextAdRequest, false);
+           
     }
   },
 
   showTopBanner() {
-    admob.createBannerView({ bannerAtTop: true });
+    admob.setOptions({ bannerAtTop: true });
+    this.showBanner();
   },
 
   showBottomBanner() {
-    admob.createBannerView({ bannerAtTop: false });
+    admob.setOptions({ bannerAtTop: false });
+    this.showBanner();
   },
 
   showBanner() {
     admob.createBannerView();
+    this.scheduleNextAdRequest();
   },
 
   hideBanner() {
     admob.destroyBannerView();
   },
+
+  /* not use right now */
+  scheduleNextAdRequest() {
+    /* make request */
+    setTimeout(() => {
+      this.requestAd();
+    }, TO);
+  },
+
+  /* not use right now */
+  requestAd() {
+    /* before making ad request, check the time eslapsed. If it is greater then
+       timeout, then make a request.
+       if app is offline, setup another timeout for detect connection status */
+    const now = new Date().getTime();
+    const diff = now - ts;
+
+    if (diff < TO) { 
+      setTimeout(() => this.requestAd(), diff);
+      return; 
+    }
+
+    /* record timestamp */
+    ts = now;
+
+    /* check network connection */
+    if (navigator && navigator.connection) {
+      const networkState = navigator.connection.type;
+      if (networkState === Connection.NONE) { // offline
+        this.scheduleNextAdRequest();
+        return;
+      }
+    }
+
+    /* manual make request */
+    // this.hideBanner();
+    this.showBanner();
+
+  }
 
 }
